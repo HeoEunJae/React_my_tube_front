0379e2936acf78df35ff77c0d3122eb37fc5e980
HeoEunJae <dmswo159@gmail.com>
2022-10-27 20:29:05 +0900

    라우터 테스트

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import About from "./routes/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
