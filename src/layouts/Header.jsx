65e01d006ca1d51cec2944de4308d054a2dc5756
HeoEunJae <dmswo159@gmail.com>
2022-10-29 17:03:48 +0900

    마이튜브 헤더 제작

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,88 +1,71 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import About from "./../routes/About";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <header>
-      <div className="navbar bg-base-100">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
+      <div className="flex justify-between navbar bg-base-100 border-b shadow-md ">
+        {/* 메인 로고 Home으로 가는 로고 시작*/}
+        <div>
+          <div
+            className="flex btn btn-ghost normal-case text-xl hover:bg-transparent"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <i className="fi fi-brands-youtube text-red-500 mt-2"></i>
+            <div className="ml-2">MyTube</div>
+          </div>
+        </div>
+        {/* 메인 로고 Home으로 가는 로고 끝*/}
+        {/* 검색창 시작*/}
+        <div className="flex-1 justify-end gap-2 mal-auto">
+          <div className="form-control mx-auto w-1/3">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered border-gray-400"
+            />
+          </div>
+        </div>
+        {/* 검색창 끝*/}
+        {/* 홈 버튼 로그아웃 버튼 등등 시작*/}
+        <div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-24 rounded-full">
+                <img src="https://placeimg.com/192/192/people" alt="profile" />
+              </div>
             </label>
             <ul
               tabIndex={0}
               className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <div>
-                  <Link to="/">Homepage</Link>
-                </div>
+                <Link to="/">
+                  <div>Homepage</div>
+                </Link>
               </li>
               <li>
-                <div>sdsdsds</div>
+                <Link to="/about">
+                  <div>about</div>
+                </Link>
               </li>
               <li>
-                <div>
-                  <Link to="/about">About</Link>
+                <div
+                  onClick={() => {
+                    navigate("/");
+                  }}
+                >
+                  Logout
                 </div>
               </li>
             </ul>
           </div>
         </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
-        </div>
+        {/* 홈 버튼 로그아웃 버튼 등등 끝*/}
       </div>
     </header>
   );
